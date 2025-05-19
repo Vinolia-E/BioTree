d873201818f8db01a19d810f402d0c5d6df338c1
Vinolia-E <vinolia.b.19@gmail.com>
2025-05-19 04:52:12 +0300

    chore: Introduce a file with the constants to create line chart svg

---
+++ svgchart/lineChartFromPairs.go
@@ -0,0 +1,24 @@
+package svgchart
+
+// Chart dimensions and styling constants
+const (
+	Width           = 600
+	Height          = 400
+	PaddingTop      = 40
+	PaddingLeft     = 60
+	PaddingRight    = 40
+	PaddingBottom   = 60
+	GridColor       = "#dddddd"
+	LineColor       = "#3366cc"
+	AxisColor       = "#333333"
+	TextColor       = "#333333"
+	BackgroundColor = "#ffffff"
+	FontSize        = 12
+	PointRadius     = 3
+)
+
+// Point represents a data point in the chart
+type Point struct {
+	X float64
+	Y float64
+}
