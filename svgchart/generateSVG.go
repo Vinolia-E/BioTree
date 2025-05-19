2bbdd201607be06418d92de94b5e42299d70da3f
Vinolia-E <vinolia.b.19@gmail.com>
2025-05-19 05:11:21 +0300

    feat: Introduce function that generates SVG

---
+++ svgchart/generateSVG.go
@@ -0,0 +1,6 @@
+package svgchart
+
+// generateSVG creates the SVG for the line chart with the given data and dimensions
+func generateSVG(data []Point, minX, maxX, minY, maxY float64, xLabel, yLabel string) string {
+	return ""
+}
