4dd2e027a6ab4a7e664a7bf519faeee76dba14bf
vandisi <vinolia.b.19@gmail.com>
2025-05-19 16:21:59 +0300

    feat: check for request type and type of document

--- docDownload/handlers.go
+++ docDownload/handlers.go
@@ -1,6 +1,24 @@
-package docdownload
+package main
 
 import "net/http"
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	
+
+	file, header, err := r.FormFile("document")
+
+	if err != nil {
+		http.Error(w, "Invalid file upload", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	if !IsSupportedFile(header.Filename) {
+		http.Error(w, "Unsupported file format", http.StatusBadRequest)
+		return
+	}
 }
