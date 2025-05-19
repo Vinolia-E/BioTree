618fff3ea35bdfdd88910d0a5d0dd1aa9a54eabe
vandisi <vinolia.b.19@gmail.com>
2025-05-19 12:45:46 +0300

    feat: Check if there is data or not

--- chart/generateLineChartSVG.go
+++ chart/generateLineChartSVG.go
@@ -19,5 +19,8 @@ type Point struct {
 
 // GenerateLineChartSVG creates an SVG string for a line chart
 func GenerateLineChartSVG(data map[string]float64) string {
+	if len(data) == 0 {
+		return `<svg xmlns="http://www.w3.org/2000/svg" width="400" height="200"><text x="10" y="20" font-size="14" fill="red">No data available</text></svg>`
+	}
 	return ""
 }
