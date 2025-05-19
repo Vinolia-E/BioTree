9c32368469a95173d39bbe49c40d4062886f88f9
vandisi <vinolia.b.19@gmail.com>
2025-05-19 12:37:30 +0300

    feat: Inroduce folder with functions handling chart generation

---
+++ chart/generateLineChartSVG.go
@@ -0,0 +1,18 @@
+package chart
+
+const (
+	width       = 400
+	height      = 200
+	padding     = 40
+	axisColor   = "#333"
+	lineColor   = "#40e0d0"
+	bgColor     = "#ffffff"
+	fontSize    = 10
+	gridColor   = "#eee"
+)
+
+// Point represents a 2D coordinate
+type Point struct {
+	X float64
+	Y float64
+}
