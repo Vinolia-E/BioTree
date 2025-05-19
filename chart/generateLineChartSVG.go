e10f747a26303c4d03dda7fdd1dc0ec11dcbfc3a
vandisi <vinolia.b.19@gmail.com>
2025-05-19 12:47:09 +0300

    feat: Sort the keys (categorical x-axis)

--- chart/generateLineChartSVG.go
+++ chart/generateLineChartSVG.go
@@ -1,14 +1,16 @@
 package chart
 
+import "sort"
+
 const (
-	width       = 400
-	height      = 200
-	padding     = 40
-	axisColor   = "#333"
-	lineColor   = "#40e0d0"
-	bgColor     = "#ffffff"
-	fontSize    = 10
-	gridColor   = "#eee"
+	width     = 400
+	height    = 200
+	padding   = 40
+	axisColor = "#333"
+	lineColor = "#40e0d0"
+	bgColor   = "#ffffff"
+	fontSize  = 10
+	gridColor = "#eee"
 )
 
 // Point represents a 2D coordinate
@@ -22,5 +24,13 @@ func GenerateLineChartSVG(data map[string]float64) string {
 	if len(data) == 0 {
 		return `<svg xmlns="http://www.w3.org/2000/svg" width="400" height="200"><text x="10" y="20" font-size="14" fill="red">No data available</text></svg>`
 	}
+
+	// Sort the keys (categorical x-axis)
+	keys := make([]string, 0, len(data))
+	for k := range data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
 	return ""
 }
