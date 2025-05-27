a2bd56b41f234c22bbbc276781422108e1851453
jkuya <kuyajesee@proton.me>
2025-05-28 00:38:45 +0300

    feat: implement main server initialization and routing

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Vinolia-E/BioTree/backend/route"
+)
+
+func main() {
+	router := route.InitRoutes()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Printf("Starting server on %s", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
