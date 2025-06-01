cb748b06d11432ea1ba0fa14d2575fe90c8bb5be
jkuya <kuyajesee@proton.me>
2025-06-01 06:02:32 +0300

    feat: add initial state management for SVG and data file handling

---
+++ frontend/static/js/state.js
@@ -0,0 +1,16 @@
+export const state = {
+  currentSvg: null,
+  currentDataFile: null,
+  chartParameters: {
+    title: "Data Visualization",
+    x_label: "Categories",
+    y_label: "Values",
+    width: 800,
+    height: 400
+  },
+  fileTypes: [
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "text/plain"
+  ]
+};
