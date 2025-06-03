4f97d4b4e983b39cfc31fd831120f1518f430b33
johneliud <johneliud4@gmail.com>
2025-06-03 22:34:44 +0300

    refactor: streamline ListDataFilesHandler by replacing ioutil with os.ReadDir and improving error handling

--- backend/handler/datafiles.go
+++ backend/handler/datafiles.go
@@ -1,68 +1,83 @@
 package handler
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "path/filepath"
-    "time"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
-    "github.com/Vinolia-E/BioTree/backend/util"
+	"github.com/Vinolia-E/BioTree/backend/util"
 )
 
 // FileInfo represents information about a data file
 type FileInfo struct {
-    Name     string    `json:"name"`
-    Size     int64     `json:"size"`
-    Modified time.Time `json:"modified"`
-    Units    []string  `json:"units"`
+	Name     string    `json:"name"`
+	Size     int64     `json:"size"`
+	Modified time.Time `json:"modified"`
+	Units    []string  `json:"units"`
 }
 
 // ListDataFilesHandler returns a list of all data files
 func ListDataFilesHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	// Set CORS headers if needed
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-    // Get all files in the data directory
-    files, err := ioutil.ReadDir("data")
-    if err != nil {
-        util.RespondError(w, "Failed to read data directory")
-        return
-    }
+	if r.Method == http.MethodOptions {
+		return
+	}
 
-    var fileInfos []FileInfo
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-    for _, file := range files {
-        // Skip directories and non-JSON files
-        if file.IsDir() || filepath.Ext(file.Name()) != ".json" {
-            continue
-        }
+	w.Header().Set("Content-Type", "application/json")
 
-        // Get file info
-        filePath := filepath.Join("data", file.Name())
-        fileInfo, err := os.Stat(filePath)
-        if err != nil {
-            continue
-        }
+	// Get all files in the data directory
+	files, err := os.ReadDir("data")
+	if err != nil {
+		log.Println("Failed to read data directory:", err)
+		util.RespondError(w, "Failed to read data directory")
+		return
+	}
 
-        // Get units from file
-        units, err := util.GetUnitsFromFile(filePath)
-        if err != nil {
-            units = []string{} // Empty array if units can't be read
-        }
+	var fileInfos []FileInfo
 
-        // Add file info to the list
-        fileInfos = append(fileInfos, FileInfo{
-            Name:     file.Name(),
-            Size:     fileInfo.Size(),
-            Modified: fileInfo.ModTime(),
-            Units:    units,
-        })
-    }
+	for _, file := range files {
+		// Skip directories and non-JSON files
+		if file.IsDir() || filepath.Ext(file.Name()) != ".json" {
+			continue
+		}
 
-    // Return the list of files
-    json.NewEncoder(w).Encode(map[string]interface{}{
-        "status": "ok",
-        "files":  fileInfos,
-    })
+		// Get file info
+		filePath := filepath.Join("data", file.Name())
+		fileInfo, err := os.Stat(filePath)
+		if err != nil {
+			continue
+		}
+
+		// Get units from file
+		units, err := util.GetUnitsFromFile(filePath)
+		if err != nil {
+			log.Printf("Failed to get units from file %s: %v", file.Name(), err)
+			units = []string{} // Empty array if units can't be read
+		}
+
+		// Add file info to the list
+		fileInfos = append(fileInfos, FileInfo{
+			Name:     file.Name(),
+			Size:     fileInfo.Size(),
+			Modified: fileInfo.ModTime(),
+			Units:    units,
+		})
+	}
+
+	// Return the list of files
+	responseWithCompression(w, r, map[string]interface{}{
+		"status": "ok",
+		"files":  fileInfos,
+	})
 }
